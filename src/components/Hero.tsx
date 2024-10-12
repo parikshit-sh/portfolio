e85dec5aef463c8dd8f63487e8f5856dbfde099f
Parikshit Sharma <parikshit.p450@gmail.com>
2024-10-12 23:26:46 +0530

    added fonts

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -17,37 +17,26 @@ const Hero = () => {
       { opacity: 1, y: 0 } // End state
     );
 
-    // Fade out on scroll
-    gsap.to(heroRef.current, {
-      opacity: 0,
-      ease: "none",
-      scrollTrigger: {
-        trigger: heroRef.current,
-        start: "top top",
-        end: "bottom top",
-        scrub: true,
-      },
-    });
   }, []);
 
   return (
     <section
       ref={heroRef}
-      className="flex items-center justify-center mb-[20vh] lg:mb-[80vh]
+      className="flex items-center justify-center md:mb-0
       min-h-screen md:min-h-[30rem] bg-transparent text-white pt-10"
     >
-      <div ref={textRef} className="text-center p-8 max-w-6xl fixed select-none">
+      <div ref={textRef} className="text-center p-8 max-w-6xl select-none">
         {/* Name Heading */}
-        <h1 className="text-[clamp(1.6rem,6vw,6rem)] font-thin font-woodland leading-[1] ">
+        <h1 className="text-[clamp(1.6rem,6vw,6rem)] font-thin font-mori leading-[1] ">
           <span className="font-gVibes font-bold">P</span>ARIKSHIT{" "}
           <span className="font-gVibes font-bold">S</span>HARMA
         </h1>
 
         {/* Title */}
-        <h1 className="text-[clamp(2rem,7vw,7rem)] uppercase font-woodland leading-[1]">
+        <h1 className="text-[clamp(2rem,7vw,7rem)] uppercase font-mori leading-[1]">
           front end 
         </h1>
-        <h1 className="text-[clamp(2rem,7vw,7rem)] uppercase font-woodland leading-[1]">
+        <h1 className="text-[clamp(2rem,7vw,7rem)] uppercase font-mori leading-[1]">
         developer<span className="font-bit">✨</span>
         </h1>
 
